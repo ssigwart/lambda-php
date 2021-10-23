e6010c169aff73bb77bf0452d252d82c246d7310
Stephen Sigwart <ssigwart@gmail.com>
2021-10-22 22:15:40 -0400

    PHP 8.0.11

--- scripts/dockerCompile.sh
+++ scripts/dockerCompile.sh
@@ -14,8 +14,8 @@ cd openssl-1.0.1k
 cd /root
 yum install -y re2c openssl openssl-devel oniguruma oniguruma-devel sqlite-devel
 mkdir php-8-bin
-curl -sL https://github.com/php/php-src/archive/php-8.0.0.tar.gz | tar -xvz
-cd php-src-php-8.0.0/
+curl -sL https://github.com/php/php-src/archive/php-8.0.11.tar.gz | tar -xvz
+cd php-src-php-8.0.11/
 ./buildconf --force
 ./configure --prefix=/root/php-8-bin/ --with-openssl=/usr/local/ssl --with-curl --with-zlib --enable-mbstring
 make install
@@ -24,6 +24,6 @@ export PATH="$(pwd)/bin/:$PATH"
 
 # Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --install-dir=./bin --filename=composer
 php -r "unlink('composer-setup.php');"
